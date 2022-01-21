d5b50fba4823d923db08a6802e345179420b9e0e
Thadd Trinh <thadtrin@amazon.com>
2022-01-20 17:51:25 -0800

    chore: simplify index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-
 const CircleMode = require("./lib/modes/CircleMode");
 const DragCircleMode = require("./lib/modes/DragCircleMode");
 const DirectMode = require("./lib/modes/DirectModeOverride");
 const SimpleSelectMode = require("./lib/modes/SimpleSelectModeOverride");
 
-exports.CircleMode = CircleMode;
-exports.DirectMode = DirectMode;
-exports.DragCircleMode = DragCircleMode;
-exports.SimpleSelectMode = SimpleSelectMode;
+module.exports = { CircleMode, DragCircleMode, DirectMode, SimpleSelectMode };
